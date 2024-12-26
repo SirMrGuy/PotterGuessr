cdd36f7e344d02d25b0cee810ddf8268f29a679a
Steven Liu <31040934+SirMrGuy@users.noreply.github.com>
2024-12-25 22:00:26 -0800

    semi-randomly pick a frame from the first movie

---
+++ extract_frames.py
@@ -0,0 +1,28 @@
+import cv2
+
+DECIMATION = 256
+MOVIE_NUMBER = 1
+
+path = f"hp{MOVIE_NUMBER}.mp4"
+cam = cv2.VideoCapture(path)
+
+# frame 
+currentframe = 0
+  
+while(True): 
+      
+    # reading from frame 
+    ret,frame = cam.read() 
+  
+    if ret:
+        if (frame_count := int(cam.get(cv2.CAP_PROP_POS_FRAMES))) % DECIMATION == 0: 
+            print(f"Writing frame {frame_count}, {cam.get(cv2.CAP_PROP_POS_MSEC)/1000/60} minutes in")
+            cv2.imwrite(f"./images/frame{MOVIE_NUMBER}-{frame_count}.jpg",
+                        cv2.resize(frame,None,fx=0.5,fy=0.5)) 
+
+    else: 
+        break
+  
+# Release all space and windows once done 
+cam.release() 
+cv2.destroyAllWindows()
